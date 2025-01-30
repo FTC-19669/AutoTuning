d7264d4a5d6a81dab03d24532da8a7eb7db13b98
ishaanko <imishaanko@gmail.com>
2025-01-29 20:49:45 -0800

    made new specimen auto

--- TeamCode/src/main/java/pedroPathing/test/SpecimenNew.java
+++ TeamCode/src/main/java/pedroPathing/test/SpecimenNew.java
@@ -23,16 +23,15 @@ public class SpecimenNew extends OpMode {
     private int pathState;
 
     private Pose startingPose = new Pose(6.661, 66.220, Math.toRadians(0));
-    private Pose scorePose = new Pose(42.300, 68.220, Math.toRadians(0));
-    private Pose pushSampleOnePose;
-    private Pose pushSampleOneEndPose;
-    private Pose pushSampleTwoPose;
-    private Pose pushSampleTwoEndPose;
-    private Pose pushSampleThreePose;
-    private Pose pushSampleThreeEndPose;
-    private Pose pickupSampleOnePose;
-    private Pose pickup;
-    private final Pose park;
+    private Pose scorePose = new Pose(43.67, 72.49, Math.toRadians(0));
+    private Pose pushSampleOnePose = new Pose(58.08, 24.95, Math.toRadians(0));
+    private Pose pushSampleOneEndPose = new Pose(14.83, 25.34, Math.toRadians(0));
+    private Pose pushSampleTwoPose = new Pose(56.6, 15.81, Math.toRadians(0));
+    private Pose pushSampleTwoEndPose = new Pose(13.56, 17.63, Math.toRadians(0));
+    private Pose pushSampleThreePose = new Pose(57.96, 11, Math.toRadians(0));
+    private Pose pushSampleThreeEndPose = new Pose(5.74, 12.56, Math.toRadians(0));
+    private Pose pickupSpecimens = new Pose(6.49, 41.79, Math.toRadians(0));
+    private final Pose parkPose = new Pose(6.49, 41.79, Math.toRadians(0));
 
     private PathChain scorePreload, pushSamples, line8, line9, line10, line11, line12, line13, line14, park;
 
@@ -51,57 +50,129 @@ public class SpecimenNew extends OpMode {
         pushSamples = follower.pathBuilder()
                 .addPath(
                         new BezierCurve(
-                                new Point(42.300, 66.220, Point.CARTESIAN),
+                                new Point(scorePose.getX(), scorePose.getY(), Point.CARTESIAN),
                                 new Point(19.788, 20.767, Point.CARTESIAN),
                                 new Point(62.302, 43.102, Point.CARTESIAN),
-                                new Point(60.931, 22.531, Point.CARTESIAN)
+                                new Point(pushSampleOnePose.getX(), pushSampleOnePose.getY(), Point.CARTESIAN)
                         )
                 )
                 .setConstantHeadingInterpolation(Math.toRadians(0))
                 .addPath(
                         new BezierLine(
-                                new Point(60.931, 22.531, Point.CARTESIAN),
-                                new Point(18.416, 22.727, Point.CARTESIAN)
+                                new Point(pushSampleOnePose.getX(), pushSampleOnePose.getY(), Point.CARTESIAN),
+                                new Point(pushSampleOneEndPose.getX(), pushSampleOneEndPose.getY(), Point.CARTESIAN)
                         )
                 )
                 .setConstantHeadingInterpolation(Math.toRadians(0))
                 .addPath(
                         new BezierCurve(
-                                new Point(18.416, 22.727, Point.CARTESIAN),
+                                new Point(pushSampleOneEndPose.getX(), pushSampleOneEndPose.getY(), Point.CARTESIAN),
                                 new Point(62.694, 24.294, Point.CARTESIAN),
-                                new Point(60.343, 12.735, Point.CARTESIAN)
+                                new Point(pushSampleTwoPose.getX(), pushSampleTwoPose.getY(), Point.CARTESIAN)
                         )
                 )
                 .setConstantHeadingInterpolation(Math.toRadians(0))
                 .addPath(
                         new BezierLine(
-                                new Point(60.343, 12.735, Point.CARTESIAN),
-                                new Point(18.024, 12.343, Point.CARTESIAN)
+                                new Point(pushSampleTwoPose.getX(), pushSampleTwoPose.getY(), Point.CARTESIAN),
+                                new Point(pushSampleTwoEndPose.getX(), pushSampleTwoEndPose.getY(), Point.CARTESIAN)
                         )
                 )
                 .setConstantHeadingInterpolation(Math.toRadians(0))
                 .addPath(
                         new BezierCurve(
-                                new Point(18.024, 12.343, Point.CARTESIAN),
+                                new Point(pushSampleTwoEndPose.getX(), pushSampleTwoEndPose.getY(), Point.CARTESIAN),
                                 new Point(59.951, 13.127, Point.CARTESIAN),
-                                new Point(59.363, 6.269, Point.CARTESIAN)
+                                new Point(pushSampleThreePose.getX(), pushSampleThreePose.getY(), Point.CARTESIAN)
                         )
                 )
                 .setConstantHeadingInterpolation(Math.toRadians(0))
                 .addPath(
                         new BezierLine(
-                                new Point(59.363, 6.269, Point.CARTESIAN),
-                                new Point(6.269, 6.269, Point.CARTESIAN)
+                                new Point(pushSampleThreePose.getX(), pushSampleThreePose.getX(), Point.CARTESIAN),
+                                new Point(pushSampleThreeEndPose.getX(), pushSampleThreeEndPose.getY(), Point.CARTESIAN)
+                        )
+                )
+                .build();
+        line8 = follower.pathBuilder()
+                .addPath(
+                        new BezierCurve(
+                                new Point(pushSampleThreeEndPose.getX(), pushSampleThreeEndPose.getY(), Point.CARTESIAN),
+                                new Point(19.984, 72.686, Point.CARTESIAN),
+                                new Point(scorePose.getX(), scorePose.getY(), Point.CARTESIAN)
+                        )
+                )
+                .build();
+        line9 = follower.pathBuilder()
+                .addPath(
+                        new BezierCurve(
+                                new Point(scorePose.getX(), scorePose.getY(), Point.CARTESIAN),
+                                new Point(19.984, 72.686, Point.CARTESIAN),
+                                new Point(pickupSpecimens.getX(), pickupSpecimens.getY(), Point.CARTESIAN)
+                        )
+                )
+                .build();
+        line10 = follower.pathBuilder()
+                .addPath(
+                        new BezierCurve(
+                                new Point(pickupSpecimens.getX(), pickupSpecimens.getY(), Point.CARTESIAN),
+                                new Point(29.976, 38.008, Point.CARTESIAN),
+                                new Point(21.355, 72.098, Point.CARTESIAN),
+                                new Point(scorePose.getX(), scorePose.getY(), Point.CARTESIAN)
+                        )
+                )
+                .build();
+        line11 = follower.pathBuilder()
+                .addPath(
+                        new BezierCurve(
+                                new Point(scorePose.getX(), scorePose.getY(), Point.CARTESIAN),
+                                new Point(21.355, 72.098, Point.CARTESIAN),
+                                new Point(29.976, 38.008, Point.CARTESIAN),
+                                new Point(pickupSpecimens.getX(), pickupSpecimens.getY(), Point.CARTESIAN)
+                        )
+                )
+                .build();
+        line12 = follower.pathBuilder()
+                .addPath(
+                        new BezierCurve(
+                                new Point(pickupSpecimens.getX(), pickupSpecimens.getY(), Point.CARTESIAN),
+                                new Point(29.976, 38.008, Point.CARTESIAN),
+                                new Point(21.355, 72.098, Point.CARTESIAN),
+                                new Point(scorePose.getX(), scorePose.getY(), Point.CARTESIAN)
+                        )
+                )
+                .build();
+        line13 = follower.pathBuilder()
+                .addPath(
+                        new BezierCurve(
+                                new Point(scorePose.getX(), scorePose.getY(), Point.CARTESIAN),
+                                new Point(21.355, 72.098, Point.CARTESIAN),
+                                new Point(29.976, 38.008, Point.CARTESIAN),
+                                new Point(pickupSpecimens.getX(), pickupSpecimens.getY(), Point.CARTESIAN)
+                        )
+                )
+                .build();
+        line14 = follower.pathBuilder()
+                .addPath(
+                        new BezierCurve(
+                                new Point(pickupSpecimens.getX(), pickupSpecimens.getY(), Point.CARTESIAN),
+                                new Point(29.976, 38.008, Point.CARTESIAN),
+                                new Point(21.355, 72.098, Point.CARTESIAN),
+                                new Point(scorePose.getX(), scorePose.getY(), Point.CARTESIAN)
                         )
                 )
                 .build();
         park = follower.pathBuilder()
                 .addPath(
-                        new BezierLine(
-                                new Point(6.269, 6.269, Point.CARTESIAN),
-                                new Point(6.269, 6.269, Point.CARTESIAN)
+                        new BezierCurve(
+                                new Point(scorePose.getX(), scorePose.getY(), Point.CARTESIAN),
+                                new Point(21.355, 72.098, Point.CARTESIAN),
+                                new Point(29.976, 38.008, Point.CARTESIAN),
+                                new Point(parkPose.getX(), parkPose.getY(), Point.CARTESIAN)
                         )
                 )
+                .build();
+
     }
 
     public void autonomousPathUpdate() {
